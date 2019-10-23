48ba07ecc20e06fe151feaa44e0f9659a87d3069
imreart <33262305+imreart@users.noreply.github.com>
2019-10-23 19:11:07 +0300

    Update Bölünme.py

--- Python/Bölünme.py
+++ Python/Bölünme.py
@@ -8,5 +8,12 @@ else:
    print(a, "sayısı tektir.")
 
 #GÖREV:Yukarıda verilen örenğin benzerini üçe bölünebilme ile yapınız:
+
+a = int(input("1. sayı: "))
+if a%3==0:
+   print(a, "sayısı 3'e tam bölünür.")
+else:
+   print(a, "sayısı 3'e tam bölünmez.")
 #!!!Python programlama dilinde boşluklar programın çalışması için önem arz etmektedir.
 #Yukarıdaki örneği baz alarak görevi yerine getirebilirsiniz.
+
